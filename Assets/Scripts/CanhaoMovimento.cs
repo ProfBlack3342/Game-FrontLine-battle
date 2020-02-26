4a53572216d82d3d486f172d50e0db6cbb0aa2b3
Lucianodasilvameyer <murcilhax@hotmail.com>
2020-02-26 15:52:35 -0300

    usar maquina de estados para pause e GameOver?

---
+++ Assets/Scripts/CanhaoMovimento.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanhaoMovimento : MonoBehaviour
+{
+   
+    Rigidbody2D righ;
+    
+
+    Vector2 mousePos;
+    public Camera cam;
+    [SerializeField]
+    private Transform Tanque;
+
+    void Awake()
+    {
+        righ = GetComponent<Rigidbody2D>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Tanque.transform.position;
+        
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        Debug.Log("Mouse position: " + mousePos);        
+    }
+    void FixedUpdate()
+    {
+        //righ.MovePosition(righ.position + moviment * moveSpeed * Time.fixedDeltaTime);
+
+        Vector2 OlharDirecao = mousePos - righ.position;
+
+        float angle = Mathf.Atan2(OlharDirecao.y,OlharDirecao.x) * Mathf.Rad2Deg-90f;
+
+        
+
+        righ.rotation = angle; 
+    }
+}
---
+++ Assets/Scripts/TanqueMovimento.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TanqueMovimento : MonoBehaviour
+{
+    public float MoveSpeed;
+    Rigidbody2D righ;
+    Vector2 moviment;
+    Animator animator;
+    void Awake()
+    {
+        righ = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+       moviment.x = Input.GetAxisRaw("Horizontal");
+       moviment.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal",moviment.x);
+        animator.SetFloat("Vertical", moviment.y);
+        animator.SetFloat("Speed", moviment.sqrMagnitude); //??
+    }
+    private void FixedUpdate()
+    {
+        righ.MovePosition(righ.position + moviment * MoveSpeed * Time.fixedDeltaTime);
+    }
+}
