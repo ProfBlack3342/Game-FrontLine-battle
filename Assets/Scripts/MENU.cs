367de4e8a7b3bb58c857702bbe8275e152e98a5e
Lucianodasilvameyer <51789104+Lucianodasilvameyer@users.noreply.github.com>
2020-03-03 12:12:33 -0300

    acertar menu

---
+++ Assets/Scripts/MENU.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MENU : MonoBehaviour
+{
+
+    private AudioClip[] AudioListners;
+    private Texture QualidadeGrafica, Resolucoes, ModoJanelaOuTelaCheia, Volume, TexturaCreditos, TexturaFundosMenu, TexturaGraficos;
+    private bool EstaNoMenuInicial, EstaNosGraficos, EstaNosCreditos;
+    public GUIStyle EstiloDosBotoesPrincipais, EstiloDosBotoesGraficos;
+    private string ResolucaoLargura = "1024", ResolucaoAltura = "768";
+    private float VOLUME;
+    private int QUALIDADEGRAFICA;
+    public Font Fonte;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
