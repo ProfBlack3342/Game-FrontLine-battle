3f1134c922b1535c5297c48b1f7ccbc404f3415e
Lucianodasilvameyer <murcilhax@hotmail.com>
2020-03-09 22:52:24 -0300

    PQ o evento de GameOver não funciona?

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -3,22 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+
+
+
 public class GameOver : MonoBehaviour
 {
 
     public event Action QuandoMorrer;
     VidaFilled vidaFilled_ref;
+    SpawnarMunicao spawnarMunicao_ref;
+    SpawnarArmadilhas spawnarArmadilhas_ref;
+    TanqueMovimento tanqueMovimento_ref;
+    CanhaoMovimento canhaoMovimento_ref;
+    //SpriteRenderer spriteRenderer_ref;
+
+    public GameObject Gameover;
 
     private void Awake()
     {
         vidaFilled_ref = GameObject.Find("Tanque").GetComponent<VidaFilled>();
+        spawnarMunicao_ref = GetComponent<SpawnarMunicao>();
+        spawnarArmadilhas_ref = GetComponent<SpawnarArmadilhas>();
+        tanqueMovimento_ref = GameObject.Find("Tanque").GetComponent<TanqueMovimento>();
+        canhaoMovimento_ref = GameObject.Find("Canhao").GetComponent<CanhaoMovimento>();
+        //spriteRenderer_ref = GameObject.Find("GameOver").GetComponent<SpriteRenderer>();
     }
     void Start()
     {
-        if (vidaFilled_ref.VidaAtual == 0)
-        {
-           // QuandoMorrer += destivarClasses();
-        } 
+        QuandoMorrer += AtivarGameOver;
     }
 
     // Update is called once per frame
@@ -26,8 +38,17 @@ public class GameOver : MonoBehaviour
     {
         
     }
-    public void destivarClasses()
+    public void AtivarGameOver()
     {
+        if (vidaFilled_ref.VidaAtual <= 0)
+        {
+            spawnarMunicao_ref.enabled = false;
+            spawnarArmadilhas_ref.enabled = false;
+            tanqueMovimento_ref.enabled = false;
+            canhaoMovimento_ref.enabled = false;
+            Gameover.SetActive(true);
+        }
 
+        
     }
 }
